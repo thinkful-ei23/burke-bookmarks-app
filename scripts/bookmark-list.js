4b369df3666f3053b3c53dd81c40ab7b4286a159
Burkey Koontz <bk.koontz@gmail.com>
2018-07-26 15:54:48 -0700

    everything in api semi-working except for bug where server loads two copies of the same item

--- scripts/bookmark-list.js
+++ scripts/bookmark-list.js
@@ -72,9 +72,9 @@ const bookmarkList = (function() {
       const newBookmark = {title, url, description, rating, expanded: false};
       // ****** need to understand how servers set ids and how to grab them
       // send bookmark to server
-      api.sendBookmarkToServer(newBookmark, () => {
+      api.sendBookmarkToServer(newBookmark, (response) => {
         store.bookmarks.createFormOpen = false;
-        store.addBookmark(newBookmark);
+        store.addBookmark(response);
         render();
       });
     });
@@ -96,13 +96,13 @@ const bookmarkList = (function() {
                   </button>
                   <form id="js-add-bookmark-form">
                       <label for="bookmark-title-entry">Add a title</label>
-                      <input required type="text" name="bookmark-title-entry" class="js-bookmark-title-entry" value="facebook">
+                      <input required type="text" name="bookmark-title-entry" class="js-bookmark-title-entry">
                       <label for="bookmark-url-entry">Add a url</label>
                       <input required type="text" name="bookmark-url-entry" class="js-bookmark-url-entry" value="https://">
                       <label for="bookmark-description-entry">Add a description</label>
                       <input type="text" name="bookmark-description-entry" class="js-bookmark-description-entry">
                       <label for="bookmark-rating">Rate your bookmark</label>
-                      <input type="number" name="bookmark-rating" class="js-bookmark-rating-entry" min="1" max="5" value="5">
+                      <input type="number" name="bookmark-rating" class="js-bookmark-rating-entry" min="1" max="5" value="1">
                     <button type="submit">Add bookmark</button>
                   </form>
                 </div>
