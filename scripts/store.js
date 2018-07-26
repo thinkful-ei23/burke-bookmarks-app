bfcf01118fdafd358b3bdb7a78a8f235bab76ccf
Burkey Koontz <bk.koontz@gmail.com>
2018-07-26 08:39:33 -0700

    store function stubs added

--- scripts/store.js
+++ scripts/store.js
@@ -3,14 +3,30 @@
 /* global $*/
 
 const store = (function() {
+  // the array of bookmark objects
+  const bookmarks = [];
+  // whether or not the create bookmark form is open
+  let createFormOpen = false;
+  // filter the results by 0 - 5. 0 does not filter any bookmarks. 2 filters by 2 or greater. 3 filters by 3 or greater, etc.
+  let filter = 0;
+
   // add a bookmark
+  const addBookmark = function() {
 
+  }
   // delete a bookmark
+  const deleteBookmark = function() {
 
-  // 
+  }
+
+  // the store will always have 
 
   return {
-
+    bookmarks,
+    createFormOpen,
+    filter,
+    addBookmark,
+    deleteBookmark
   };
 
 }());
