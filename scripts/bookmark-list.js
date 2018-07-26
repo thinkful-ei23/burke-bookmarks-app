16518bdf6d8ba64ae1e916d086934702458988cc
Burkey Koontz <bk.koontz@gmail.com>
2018-07-26 09:16:32 -0700

    commented about event handlers that the project will need

--- scripts/bookmark-list.js
+++ scripts/bookmark-list.js
@@ -3,7 +3,19 @@
 /* global $*/
 
 const bookmarkList = (function() {
+  // need event handlers for everything the user can click on in the dom
 
+  // event handler for the drop down filter menu
+
+  // event handler for the create element button (set on the create button itself)
+
+  // event handler for the submit on the create element form
+
+  // event handler for the delete element button (set on the x button that is a part of the form)
+
+  // event handler for the expand bookmark feature (set on the ul)
+
+  // render function
 
   return {
 
